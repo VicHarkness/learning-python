f6b43ed6f719f81908b6e82dbfcdae475e82d314
vicness <vicharkness3d@gmail.com>
2019-01-03 11:04:18 +0000

    Updated number guessing game

--- guessinggame.py
+++ guessinggame.py
@@ -3,12 +3,11 @@ import random
 targetnumber=random.randint(1,100)
 guess=0
 
-while (True):
+while True:
     if (guess != targetnumber):
         guess=int(input("Guess a number from 1 to 100 "))
         if (guess == targetnumber):
             print("You guessed correctly, congratulations!")
-            False
             break
         if (guess > targetnumber):
             print("You guessed too high")
